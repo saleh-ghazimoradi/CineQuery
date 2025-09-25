66bac628609f25173197b07045c3eb435bc2639f
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-09-25 14:06:38 +0330

    create a movie model to store movie related data in the postgresql

---
+++ internal/domain/movie.go
@@ -0,0 +1,13 @@
+package domain
+
+import "time"
+
+type Movie struct {
+	ID        int64
+	CreatedAt time.Time
+	Title     string
+	Year      int32
+	Runtime   int32
+	Genres    []string
+	Version   int32
+}
